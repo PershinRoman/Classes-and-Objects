5243ecb2b216413a03010038bd4d3d427cf18978
Roman <sqrap@mail.ru>
2025-03-10 20:57:13 +0300

    lesson 16.2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 import logging
+from abc import ABC, abstractmethod
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class Product:
+class BaseProduct(ABC):
+    @abstractmethod
+    def price(self):
+        pass
+
+    @abstractmethod
+    def new_product(self):
+        pass
+
+    @abstractmethod
+    def __string__(self):
+        pass
+
+
+class Product(BaseProduct):
 
     def __init__(self, name, description, price, quantity):
         self.name = name
@@ -149,6 +164,15 @@ class Categorypro:
             return product
         else:
             raise StopIteration
+
+
+class MixinsProduct(Product):
+    ID = 1
+
+    def __init__(self):
+        super().__init__()
+        self.id = id
+        MixinsProduct.ID += 1
 
 
 if __name__ == "__main__":
@@ -348,3 +372,46 @@ if __name__ == '__main__':
         print("Возникла ошибка TypeError при добавлении не продукта")
     else:
         print("Не возникла ошибка TypeError при добавлении не продукта")
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
+    print(product1.price)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
+
+    category1 = Categoryiter("Смартфоны",
+                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         [product1, product2, product3])
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category2 = Categoryiter("Телевизоры",
+                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+                         [product4])
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Categoryiter.category_count)
+    print(Categoryiter.product_count)
